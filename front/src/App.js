8d73e1d2a6c026f6ddec9df05c008d6df5b3db37
Polina Chubenko <chubenko.pn@phystech.edu>
2023-04-14 13:46:15 +0300

    fixes

--- front/src/App.js
+++ front/src/App.js
@@ -1,18 +1,13 @@
 //import logo from './logo.svg';
 import './App.css';
-import Balloon from './component/balloon_feature/balloon_feature'
+import Balloon from './component/Balloon/Balloon'
 
 // Balloon и  Balloon2 недописано
 function App() {
   return (
     <div className="App">
-        <div style={{ position: "absolute", left: 40, top: 120 }}>
-            <Balloon
-                title_pl1="Первый игрок"
-                name_pl1="Артемий Клячин"
-                handicap_pl1={14}
-                club_mem_pl1={"да"}
-            />
+        <div>
+            <Balloon/>
         </div>
     </div>
   );
