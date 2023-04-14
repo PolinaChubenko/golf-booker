e28bbd877008e00c2b036cd2f96502a4c8dda0c8
Polina Chubenko <chubenko.pn@phystech.edu>
2023-04-14 14:05:46 +0300

    folder fixes

--- front/src/App.js
+++ front/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Balloon from './component/Balloon/Balloon'
+import Balloon from './components/Balloon/Balloon'
 import Calendar from "./components/Calendar/Calendar";
 
 function App() {
