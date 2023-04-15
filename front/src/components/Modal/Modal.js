6689efc92f185e3da88188da85df398ea4b0112b
Polina Chubenko <chubenko.pn@phystech.edu>
2023-04-16 01:30:58 +0300

    events colored conditionally + modal in progress

--- front/src/components/Modal/Modal.js
+++ front/src/components/Modal/Modal.js
@@ -1,14 +1,35 @@
 import style from "./Modal.module.css"
+import React, {useState} from "react";
+import moment from "moment/moment";
+import InputSection from "../InputSection/InputSection";
 
-const Modal = ({ handleClose, show, children }) => {
+const Modal = ({ handleClose, show, eventId }) => {
     const showHideClassName = show ? style.display_block : style.display_none;
+
+    const [teeTime, setTeeTime] = useState(null)
+
+    const setTeeTimeForModal= (time) => {
+        const teeTime = moment(time).format("DD.MM, HH:mm");
+        setTeeTime(teeTime)
+    }
 
     return (
         <div className={[showHideClassName, style.modal].join(" ")}>
             <section className={style.modal_main}>
-                {children}
+                <div className={style.tee_time_wrapper}>
+                    <p>Слот 14.04 на время 10:30</p>
+                </div>
+                <div className={style.players_wrapper}>
+                    <p>Игроки</p>
+                </div>
+                <div>
+                    <InputSection />
+                </div>
                 <button type="button" onClick={handleClose}>
-                    Close
+                    Отменить
+                </button>
+                <button type="button" onClick={handleClose}>
+                    Сохранить
                 </button>
             </section>
         </div>
