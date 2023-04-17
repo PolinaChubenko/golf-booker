a9f3fae29a0bbdff4c8b9f01cd23a2f6e4d9467a
artemiy <artklyachin@gmail.com>
2023-04-17 21:58:16 +0300

    commented checkbox

--- front/src/components/CheckBox/CheckBox.js
+++ front/src/components/CheckBox/CheckBox.js
@@ -1,17 +1,19 @@
 import React from 'react';
 
-const updateInput = (ref, checked) => {
+const updateInput = (ref, checked) => { // Синхронизация состояния checked с ref
     const input = ref.current;
     if (input) {
-        input.checked = checked;
-        input.indeterminate = checked == null;
+        input.checked = checked; // галочка, если true
+        input.indeterminate = checked == null; // минус, если null
+        // ничего, если false
     }
 };
 
+// Рассчитан только на три состояния
 const ThreeStateCheckbox = ({name, checked, onChange}) => {
-    const inputRef = React.useRef(null); // Меняющееся значение checkbox
-    const checkedRef = React.useRef(checked);
-    React.useEffect(() => { // вызывается, епри рендере или если изменился checked
+    const inputRef = React.useRef(null); // Состояние checkbox
+    const checkedRef = React.useRef(checked); // Состояние для работы, синхронизируем с inputRef
+    React.useEffect(() => { // вызывается, при создании checkbox (задание начального состояния)
         checkedRef.current = checked;
         updateInput(inputRef, checked);
     }, [checked]);
@@ -28,17 +30,21 @@ const ThreeStateCheckbox = ({name, checked, onChange}) => {
                 break;
         }
         updateInput(inputRef, checkedRef.current);
-        if (onChange) {
-            onChange(checkedRef.current);
-        }
+        // if (onChange) {
+        //     onChange(checkedRef.current);
+        // }
     };
     return (
         <input ref={inputRef} type="checkbox" name={name} onClick={handleClick} />
     );
 };
 
+// 1 : false (пустое)
+// 2 : true (галочка)
+// 3 : null (минус)
+
 export const CheckBox = () => {
-    const [checked] = React.useState(false); //  Значение по умолчанию null
+    const [checked] = React.useState(true); // Значение по умолчанию
 
     return (
         <ThreeStateCheckbox checked={checked} />
