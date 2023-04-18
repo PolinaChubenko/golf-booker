fe918fc8baab28f322efa591a3b0d51dce12ebbd
artemiy <artklyachin@gmail.com>
2023-04-18 11:19:46 +0300

    reset App to initial state

--- front/src/App.js
+++ front/src/App.js
@@ -5,17 +5,17 @@ import User from "./components/PageUser/User";
 import Admin from "./components/PageAdmin/Admin";
 
 function App() {
-    return (
-        <div className="App">
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<User />} />
-                    <Route path="/admin" element={<Admin />} />
-                    <Route path="*" element={<NotFound />} />
-                </Routes>
-            </BrowserRouter>
-        </div>
-    );
+  return (
+    <div className="App">
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<User />} />
+                <Route path="/admin" element={<Admin />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
