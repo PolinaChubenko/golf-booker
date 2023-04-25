f582eb1991806c1cf5d5e961c25afd1b8cb5b69a
Egor Sbrodov <egor-ulstu@yandex.ru>
2023-04-25 12:45:32 +0300

    Docker feature (#37)
    * commit docker-compose
    
    * commit changes
    
    * commit old docker-compose
    
    * docker configured
    
    * added gunicron
    
    * change gunicorn tmp dir
    
    * changed workers count
    
    * wsgi:app added
    
    * more timeout added
    
    * changed timeout
    
    * configured DB host
    
    * fix db hostname
    
    * Deleted useless compose
    
    * change react build
    
    * support React Router
    
    * Nginx conf moved
    
    * try twice
    
    * Remove docker for front

--- back/config.py
+++ back/config.py
@@ -1,6 +1,9 @@
+import os
+
 HOST = "127.0.0.1"
 PORT = 8080
 DEBUG = True
 
-DB_HOST = "mongodb://localhost:27017/"
+DB_HOSTNAME = os.environ.get("MONGODB_HOST", "localhost")
+DB_HOST = f"mongodb://{DB_HOSTNAME}:27017/"
 DB_NAME = "golfbooker"
